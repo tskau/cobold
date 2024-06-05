c6c3e0e1cd7d7398ca20209bc41fe4cdc218912a
Damir Modyarov <otomir@yandex.ru>
2024-06-05 18:30:59 +0300

    fix: Update default API_BASE_URL env variable value

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,7 @@ export const env = createEnv({
         BOT_TOKEN: z.string(),
         INLINE_FIX_CHAT_ID: z.coerce.number().int(),
         API_BASE_URL: z.string()
-            .default("https://co.wuk.sh/api")
+            .default("https://api.cobalt.tools/api")
             .transform(s => s.split(";"))
             .pipe(z.array(z.string().url())),
         SELECT_TYPE_PHOTO_URL: z.string().url().default("https://i.otomir23.me/buckets/cobold/download.png"),
