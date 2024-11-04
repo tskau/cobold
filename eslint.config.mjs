052d38f8c2e58c164cc350ce770b305073dc1c72
Damir Modyarov <otomir@yandex.ru>
2024-11-04 22:42:12 +0300

    chore: Migrate to antfu's eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import antfu from "@antfu/eslint-config"
+
+export default antfu({
+    stylistic: {
+        indent: 4,
+        quotes: "double",
+    },
+    typescript: {
+        overrides: {
+            "@typescript-eslint/consistent-type-definitions": ["error", "type"],
+        },
+    },
+    jsonc: false,
+}, {
+    rules: {
+        "perfectionist/sort-imports": ["error", {
+            groups: [
+                "type",
+                ["parent-type", "sibling-type", "index-type"],
+
+                "builtin",
+                "external",
+                ["internal", "internal-type"],
+                ["parent", "sibling", "index"],
+                "side-effect",
+                "object",
+                "unknown",
+            ],
+            newlinesBetween: "ignore",
+            order: "asc",
+            type: "natural",
+            internalPattern: ["@/**"],
+        }],
+        "style/member-delimiter-style": ["error", {
+            multiline: {
+                delimiter: "comma",
+                requireLast: true,
+            },
+            singleline: {
+                delimiter: "comma",
+                requireLast: false,
+            },
+        }],
+        "no-console": ["off"],
+        "style/brace-style": ["error", "1tbs", {
+            allowSingleLine: true,
+        }],
+        "antfu/top-level-function": ["off"],
+    },
+})
