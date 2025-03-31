8fbe4617081e2ccdd6ab0af749817e9e6b9c7384
Damir Modyarov <otomir@yandex.ru>
2025-03-31 19:26:06 +0300

    feat: Improve tunneling error handling

--- src/core/data/cobalt/external.ts
+++
@@ -1,5 +0,0 @@
-import { baseFetch } from "@/core/data/cobalt/common"
-
-export async function retrieveExternalMedia(url: string, proxy?: string) {
-    return baseFetch(url, { proxy }).then(r => r.arrayBuffer())
-}
--- src/core/data/cobalt/tunnel.ts
+++ src/core/data/cobalt/tunnel.ts
@@ -3,22 +3,25 @@ import { genericErrorSchema, getErrorText } from "@/core/data/cobalt/error"
 import type { Result } from "@/core/utils/result"
 import { error, ok } from "@/core/utils/result"
 import type { Text } from "@/core/utils/text"
+import { translatable } from "@/core/utils/text"
 
 export async function retrieveTunneledMedia(url: string, proxy?: string): Promise<Result<ArrayBuffer, Text>> {
     const data = await baseFetch(url, { proxy })
+        .catch(() => null)
+    if (!data)
+        return error(translatable("error-unresponsive"))
 
     if (!data.ok) {
         const content = await data.json().catch(() => null) as unknown
         const body = genericErrorSchema.safeParse(content)
-        if (!body.success) {
-            throw new Error(`streaming from ${new URL(url).host} failed`)
-        }
+        if (!body.success)
+            return error(translatable("error-invalid-response"))
         return error(getErrorText(body.data.error.code))
     }
 
     const buffer = await data.arrayBuffer()
     if (!buffer.byteLength)
-        throw new Error(`empty body from ${new URL(url).host}`)
+        return error(translatable("error-invalid-response"))
 
     return ok(buffer)
 }
