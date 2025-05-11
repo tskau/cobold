600c4793ee1d473fa2efe0511dff2083a743fc55
Damir Modyarov <otomir@yandex.ru>
2025-05-11 19:38:34 +0300

    [object Object]

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import process from "node:process"
 import { startBot } from "@/telegram/bot"
 import { log } from "@/telegram/helpers/debug"
 
-log(`process started, avm: ${process.availableMemory()}, cpu: ${process.cpuUsage()}`)
+log(`process started, avm: ${process.availableMemory()}, cpu: ${process.cpuUsage().system}`)
 void startBot()
